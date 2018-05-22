b93af69293f367934365a06cd169295344221542
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-05-22 14:55:23 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,16 @@
-CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, cateogry TEXT, funding_goal INTEGER, start_date DATE, end_date DATE);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal NUMERIC, start_date TEXT, end_date TEXT);
 
-CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+)
+;
 
-CREATE TABLE pledge (amount INTEGER);
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount NUMERIC,
+user_id INTEGER,
+project_id INTEGER
+)
+;
