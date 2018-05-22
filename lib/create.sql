fa5626db621621ffb6b8b9705a4abdcb91e94030
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-05-22 14:51:12 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,1 @@
 CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, cateogry TEXT, funding_goal INTEGER, start_date DATE, end_date DATE);
-
-    it "creates a projects table with a start_date field" do
-      expect{@db.execute("SELECT start_date FROM projects;")}.to_not raise_exception
-    end
-
-    it "creates a projects table with a end_date field" do
-      expect{@db.execute("SELECT end_date FROM projects;")}.to_not raise_exception
-    end
